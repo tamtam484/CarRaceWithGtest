038bc93df2bfcf84ccde060ba40378d10256b5bb
tamtam484 <tamura.y.ap@m.titech.ac.jp>
2017-07-02 12:30:29 +0900

    added gtest file (to be written)

---
+++ gtest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gtest
+//
+//  Created by 田村雄太 on 2017/07/02.
+//  Copyright © 2017年 田村雄太. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ gtest/main.hpp
@@ -0,0 +1,14 @@
+//
+//  main.hpp
+//  Car
+//
+//  Created by 田村雄太 on 2017/07/02.
+//  Copyright © 2017年 田村雄太. All rights reserved.
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
